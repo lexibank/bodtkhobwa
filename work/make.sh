c0d1bc68f5431bd5d2dcd54274b05444ec2bb7a8
lingulist <mattis.list@lingpy.org>
2018-07-24 13:35:03 +0200

    update concept sets

---
+++ work/make.sh
@@ -0,0 +1,5 @@
+# Manually created the concepts-unmapped.tsv file with VI, then linked it automatically with pyconcepticon
+
+$ concepticon map_concepts concepts-unmapped.tsv > concepts-mapped.tsv
+
+# then it needs to be refined manually, we'll instruct Tim Bodt how to do this.
