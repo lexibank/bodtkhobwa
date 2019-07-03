1b279561ab5f7e18500350251275ecba6050d1bb
Christoph Rzymski <christoph@functor-argument.net>
2019-07-03 14:29:01 +0200

    Code cleanup, newly generated CLDF, Glottolog 4.0, release 1.1.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This dataset is licensed under a https://creativecommons.org/licenses/by-nc/4.0/
 
 ## Notes
 
-This dataset was prepared from a collection of original field work data by TTimotheus A. Bodt, during an intensive week of collaboration with Johann-Matttis List and the [CALC](http://calc.digling.org) project in July 2018. The data comprises 8 varieties of Kho-Bwa in phonetic transcription and with explicit segmentation of morphemes.
+This dataset was prepared from a collection of original field work data by Timotheus A. Bodt, during an intensive week of collaboration with Johann-Matttis List and the [CALC](http://calc.digling.org) project in July 2018. The data comprises 8 varieties of Kho-Bwa in phonetic transcription and with explicit segmentation of morphemes.
 
 
 
--- lexibank_bodtkhobwa.py
+++ lexibank_bodtkhobwa.py
@@ -1,11 +1,8 @@
-from __future__ import unicode_literals, print_function
-
 import attr
 from clldutils.path import Path
 from csvw import Datatype
-from pylexibank.dataset import NonSplittingDataset, Cognate
-
 from lingpy import *
+from pylexibank.dataset import NonSplittingDataset, Cognate
 from tqdm import tqdm
 
 
@@ -15,7 +12,7 @@ class KhobwaCognate(Cognate):
 
 
 class Dataset(NonSplittingDataset):
-    id = 'bodtkhobwa'
+    id = "bodtkhobwa"
     dir = Path(__file__).parent
     cognate_class = KhobwaCognate
 
@@ -24,121 +21,118 @@ class Dataset(NonSplittingDataset):
 
     def cmd_install(self, **kw):
 
-        data = Wordlist(self.dir.joinpath('raw',
-            'bodt-khobwa-cleaned.tsv').as_posix(),
-            conf=self.raw.posix('wordlist.rc'))
-        langs = {} # need for checking later
+        wl = Wordlist(
+            self.dir.joinpath("raw", "bodt-khobwa-cleaned.tsv").as_posix(),
+            conf=self.raw.posix("wordlist.rc"),
+        )
+        langs = {}  # need for checking later
         concepts = {}
 
         with self.cldf as ds:
             for concept in self.concepts:
                 ds.add_concept(
-                        ID=concept['NUMBER'],
-                        Name=concept['ENGLISH'],
-                        Concepticon_ID=concept['CONCEPTICON_ID'],
-                        Concepticon_Gloss=concept['CONCEPTICON_GLOSS']
-                        )
-                concepts[concept['ENGLISH']] = concept['NUMBER']
+                    ID=concept["NUMBER"],
+                    Name=concept["ENGLISH"],
+                    Concepticon_ID=concept["CONCEPTICON_ID"],
+                    Concepticon_Gloss=concept["CONCEPTICON_GLOSS"],
+                )
+                concepts[concept["ENGLISH"]] = concept["NUMBER"]
 
             for language in self.languages:
                 ds.add_language(
-                        ID=language['ID'],
-                        Glottocode=language['Glottolog'],
-                        Name=language['Name'],
-                        )
-                langs[language['ID']] = language
+                    ID=language["ID"], Glottocode=language["Glottolog"], Name=language["Name"]
+                )
+                langs[language["ID"]] = language
 
             ds.add_sources(*self.raw.read_bib())
             num = 580
-            for concept in data.concepts:
-                if not concept in concepts:
+            for concept in wl.concepts:
+                if concept not in concepts:
                     print('"{0}","{1}",,,'.format(num, concept))
                     num += 1
 
             mapper = {
-                    "pʰl": "pʰ l",
-                    "aw": "au",
-                    "ɛj": "ɛi",
-                    "ɔw": "ɔu",
-                    "bl": "b l",
-                    "aj": "ai",
-                    "ɔj": "ɔi",
-                    "(ŋ)": "ŋ",
-                    "kʰl": "kʰ l",
-                    "kl": "k l",
-                    "ej": "ei",
-                    "uj": "ui",
-                    "bɹ": "b ɹ",
-                    "ɐʰ": "ɐʰ/ɐ",
-                    "hw": "h w",
-                    "ɔːʰ": "ɔːʰ/ɔː",
-                    "dʑr": "dʑ r",
-                    "ow": "ou",
-                    "pl": "p l",
-                    "lj": "l j",
-                    "tʰj": "tʰ j",
-                    "aːʰ": "aːʰ/aː",
-                    "bj": "b j",
-                    "mp": "m p",
-                    "pɹ": "p ɹ",
-                    "ɐ̃ʰ": "ɐ̃ʰ/ɐ̃",
-                    "ɔ̃ʰ": "ɔ̃ʰ/ɔ̃",
-                    "aj~e/aj": "aj~e/ai",
-                    "aj~ej/ej": "aj~ej/ei",
-                    "kl": "k l",
-                    "kʰɹ": "kʰ ɹ",
-                    "ɛːʰ":"ɛːʰ/ɛː",
-                    "ɔʰ": "ɔʰ/ɔ",
-                    "tɹ": "t ɹ",
-                    "ɐːʰ": "ɐːʰ/ɐ",
-                    "br": "b r",
-                    "kɹ": "k ɹ",
-                    "kʰj": "kʰ j",
-                    "kʰr": "kʰ r",
-                    "gɹ": "g ɹ",
-                    "hj": "h j",
-                    "bl~gl/bl": "bl~gl/b l",
-                    "dj": "d j",
-                    "ej~i/ej": "ej~i/ei",
-                    "e~a/ej": "e~a/ei",
-                    "fl": "f l",
-                    "kʰw": "kʰ w",
-                    "mj": "m j",
-                    "pr": "p r",
-                    "pʰl~bl/pʰl": "pʰl~bl/pʰ l",
-                    "pʰr": "pʰ r",
-                    "pʰr~pʰl/pʰr": "pʰr~pʰl/pʰ r",
-                    "pʰw": "pʰ w",
-                    "pʰɹ": "pʰ ɹ",
-                    "tr": "t r",
-                    "tɕʰɹ": "tɕʰ ɹ",
-                    "tʰr": "tʰ r",
-                    "tʰw": "tʰ w",
-                    "zj": "z j",
-                    "ɔj~uj/uj": "ɔj~uj/ui",
-                    }
+                "pʰl": "pʰ l",
+                "aw": "au",
+                "ɛj": "ɛi",
+                "ɔw": "ɔu",
+                "bl": "b l",
+                "aj": "ai",
+                "ɔj": "ɔi",
+                "(ŋ)": "ŋ",
+                "kʰl": "kʰ l",
+                "ej": "ei",
+                "uj": "ui",
+                "bɹ": "b ɹ",
+                "ɐʰ": "ɐʰ/ɐ",
+                "hw": "h w",
+                "ɔːʰ": "ɔːʰ/ɔː",
+                "dʑr": "dʑ r",
+                "ow": "ou",
+                "pl": "p l",
+                "lj": "l j",
+                "tʰj": "tʰ j",
+                "aːʰ": "aːʰ/aː",
+                "bj": "b j",
+                "mp": "m p",
+                "pɹ": "p ɹ",
+                "ɐ̃ʰ": "ɐ̃ʰ/ɐ̃",
+                "ɔ̃ʰ": "ɔ̃ʰ/ɔ̃",
+                "aj~e/aj": "aj~e/ai",
+                "aj~ej/ej": "aj~ej/ei",
+                "kl": "k l",
+                "kʰɹ": "kʰ ɹ",
+                "ɛːʰ": "ɛːʰ/ɛː",
+                "ɔʰ": "ɔʰ/ɔ",
+                "tɹ": "t ɹ",
+                "ɐːʰ": "ɐːʰ/ɐ",
+                "br": "b r",
+                "kɹ": "k ɹ",
+                "kʰj": "kʰ j",
+                "kʰr": "kʰ r",
+                "gɹ": "g ɹ",
+                "hj": "h j",
+                "bl~gl/bl": "bl~gl/b l",
+                "dj": "d j",
+                "ej~i/ej": "ej~i/ei",
+                "e~a/ej": "e~a/ei",
+                "fl": "f l",
+                "kʰw": "kʰ w",
+                "mj": "m j",
+                "pr": "p r",
+                "pʰl~bl/pʰl": "pʰl~bl/pʰ l",
+                "pʰr": "pʰ r",
+                "pʰr~pʰl/pʰr": "pʰr~pʰl/pʰ r",
+                "pʰw": "pʰ w",
+                "pʰɹ": "pʰ ɹ",
+                "tr": "t r",
+                "tɕʰɹ": "tɕʰ ɹ",
+                "tʰr": "tʰ r",
+                "tʰw": "tʰ w",
+                "zj": "z j",
+                "ɔj~uj/uj": "ɔj~uj/ui",
+            }
 
             # add data to cldf
-            ds['FormTable', 'Segments'].separator = ' + '
-            ds['FormTable', 'Segments'].datatype = Datatype.fromvalue({
-                "base": "string",
-                "format": "([\\S]+)( [\\S]+)*"
-            })
-            for idx in tqdm(data, desc='cldf the data'):
-                segments = ' '.join([mapper.get(x, x) for x in data[idx, 'tokens']])
-                morphemes = segments.split(' + ')
-                concept = concepts.get(data[idx, 'concept'], '')
+            ds["FormTable", "Segments"].separator = " + "
+            ds["FormTable", "Segments"].datatype = Datatype.fromvalue(
+                {"base": "string", "format": "([\\S]+)( [\\S]+)*"}
+            )
+            for idx in tqdm(wl, desc="cldf the data"):
+                segments = " ".join([mapper.get(x, x) for x in wl[idx, "tokens"]])
+                morphemes = segments.split(" + ")
+                concept = concepts.get(wl[idx, "concept"], "")
                 for lex in ds.add_lexemes(
-                    Language_ID=data[idx, 'doculect'].lower(),
+                    Language_ID=wl[idx, "doculect"].lower(),
                     Parameter_ID=concept,
-                    Form=data[idx, 'form'],
-                    Value=data[idx, 'value'],
+                    Form=wl[idx, "form"],
+                    Value=wl[idx, "value"],
                     Segments=morphemes,
-                    Source=['Bodt2019'],
+                    Source=["Bodt2019"],
                 ):
-                    for morpheme_index, cogid in enumerate(data[idx, 'crossids']):
-                        alignment = data[idx, 'alignment'].split(' + ')[morpheme_index].split()
-                        alignment = ' '.join([mapper.get(x, x) for x in alignment]).split()
+                    for morpheme_index, cogid in enumerate(wl[idx, "crossids"]):
+                        alignment = wl[idx, "alignment"].split(" + ")[morpheme_index].split()
+                        alignment = " ".join([mapper.get(x, x) for x in alignment]).split()
                         if int(cogid):
                             ds.add_cognate(
                                 lexeme=lex,
